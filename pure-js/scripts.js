17b351b9c6768e399527b54f8bf2421221753c5f
jackyan63@gmail.com <jackyan63@gmail.com>
2018-04-10 23:09:49 -0400

    Add html and js

---
+++ pure-js/scripts.js
@@ -0,0 +1,12 @@
+function carousel (){
+	// UI
+    	//Image container
+    	//Prev next button
+    	//Bullet point
+
+
+    //Setting
+    	// 1. Photolist: array
+    	// 2. Circular: true
+    	// 3.
+}
